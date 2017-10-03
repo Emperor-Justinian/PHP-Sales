4ec5f9373931eda1194d16647a16857c24836fdd
James Crawford <jimmyson.2007@gmail.com>
2017-10-03 14:16:04 +1100

    Added Chart.mvc for Plotting

--- PHP.Sales.DataAccess/Configurations/ReportConfiguration.cs
+++ PHP.Sales.DataAccess/Configurations/ReportConfiguration.cs
@@ -10,8 +10,8 @@ namespace PHP.Sales.DataAccess.Configurations
             Property(x => x.Name)
                 .HasMaxLength(100)
                 .IsRequired();
-            Property(x => x.ProductID)
-                .IsRequired();
+            /*Property(x => x.ProductID)
+                .IsRequired();*/
             Property(x => x.Start)
                 .IsRequired();
             Property(x => x.End)
